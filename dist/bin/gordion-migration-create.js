f6ac0a117b423f29072e732215481d196d77ae9e
vukbgit <info@vuk.bg.it>
2021-09-13 16:54:39 +0200

    bin executables introduction

---
+++ dist/bin/gordion-migration-create.js
@@ -0,0 +1,5 @@
+"use strict";
+
+var _logger = require("../logger");
+
+_logger.logger.info('GORDION-MIGRATION-CREATE SCRIPT RUNNING!');
---
+++ src/bin/gordion-migration-create.ts
@@ -0,0 +1,3 @@
+import { logger } from '../logger'
+
+logger.info('GORDION-MIGRATION-CREATE SCRIPT RUNNING!')
