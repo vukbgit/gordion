fdd0041f4822d2621e43f900a75839fb702c501b
vukbgit <info@vuk.bg.it>
2021-09-19 09:40:10 +0200

    compile binary

--- dist/bin/compile-gordion.js
+++ dist/bin/compile-gordion.js
@@ -3,12 +3,17 @@
 
 var _commander = require("commander");
 
+var _logger = require("../logger");
+
 var _shellCommander = require("../shell-commander");
 
 const program = new _commander.Command();
-//logger.setLevel('INFO')
-program.description('compiles Gordion package').action((options, command) => {
-  _shellCommander.shellCommander.exec('npm run compile', {
+program.description('compiles Gordion package').option('-c, --clean', 'cleans dist folder before compiling').option('-t, --test <fake>', 'test').action((options, command) => {
+  _logger.logger.info(options.clean);
+
+  const compileCommand = typeof options.clean != 'undefined' && options.clean === true ? 'npm run compile-clean' : 'npm run compile'; //const compileCommand = 'npm run compile'
+
+  _shellCommander.shellCommander.exec(compileCommand, {
     'cwd': 'node_modules/gordion'
   });
 }).parse();
--- src/bin/compile-gordion.ts
+++ src/bin/compile-gordion.ts
@@ -1,15 +1,19 @@
 #!/usr/bin/env node
-import { Command } from 'commander';
+import { Command,Option } from 'commander';
 const program = new Command();
+import { logger } from '../logger'
 import { shellCommander } from '../shell-commander'
-
-//logger.setLevel('INFO')
 
 program
   .description('compiles Gordion package')
-  .action((options: [string], command: Command) => {
+  .option('-c, --clean', 'cleans dist folder before compiling')
+  .option('-t, --test <fake>', 'test')
+  .action((options, command: Command) => {
+    logger.info(options.clean)
+    const compileCommand = typeof options.clean != 'undefined' && options.clean === true ? 'npm run compile-clean' : 'npm run compile'
+    //const compileCommand = 'npm run compile'
     shellCommander.exec(
-      'npm run compile',
+      compileCommand,
       {
         'cwd': 'node_modules/gordion',
       }
