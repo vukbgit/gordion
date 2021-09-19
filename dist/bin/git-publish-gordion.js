9592025108212edd3b1103b8464491b26a62f7cc
vukbgit <info@vuk.bg.it>
2021-09-19 19:10:54 +0200

    git publisher

--- dist/bin/git-publish-gordion.js
+++ dist/bin/git-publish-gordion.js
@@ -3,72 +3,13 @@
 
 var _commander = require("commander");
 
-var _enquirer = require("enquirer");
-
-var _shellCommander = require("../shell-commander");
+var _gitPublisher = require("../git-publisher");
 
 const program = new _commander.Command();
 
 (async () => {
-  program.description('Publish updates to Gordion GIT repository').action(publishToGIT);
+  program.description('Publish updates to Gordion GIT repository').action((name, options, command) => {
+    _gitPublisher.gitPublisher.publishToGIT('gordion');
+  });
   await program.parseAsync();
 })();
-
-async function gitStatus() {
-  const status = await _shellCommander.shellCommander.exec('cd node_modules/gordion && git status');
-  return status;
-}
-
-async function selectFilesToPublish() {
-  const result = await _shellCommander.shellCommander.exec('cd node_modules/gordion && git diff --name-only', {}, true);
-  let files = result.stdout.trim().split('\n');
-
-  if (files.length == 0 || files.length == 1 && files[0] == '') {
-    return false;
-  } else {
-    files.unshift('ALL');
-  }
-
-  try {
-    const input = await (0, _enquirer.prompt)({
-      type: 'multiselect',
-      name: 'selectedFiles',
-      //initial: true,
-      message: 'Select files to be published (ESC to abort)',
-      choices: files
-    });
-
-    if (input.selectedFiles.indexOf('ALL') !== -1) {
-      input.selectedFiles = result.stdout.split(/\r?\n/);
-    }
-
-    return input.selectedFiles;
-  } catch (e) {
-    return false;
-  }
-}
-
-async function askGitCommitMessage() {
-  const input = await (0, _enquirer.prompt)({
-    type: 'input',
-    name: 'message',
-    initial: 'Gordion commit',
-    message: 'GIT commit message'
-  });
-  return input.message;
-}
-
-async function gitPublish(filesToPublish, message) {
-  const commit = await _shellCommander.shellCommander.exec('cd node_modules/gordion && git add ' + filesToPublish.join(' ') + ' && git commit -m "' + message + '" && git push');
-  return commit;
-}
-
-async function publishToGIT() {
-  await gitStatus();
-  const filesToPublish = await selectFilesToPublish();
-
-  if (filesToPublish !== false) {
-    const message = await askGitCommitMessage();
-    const commit = await gitPublish(filesToPublish, message);
-  }
-}
--- src/bin/git-publish-gordion.ts
+++ src/bin/git-publish-gordion.ts
@@ -1,68 +1,13 @@
 #!/usr/bin/env node
-import * as path from 'path';
 import { Command } from 'commander';
 const program = new Command();
-import { prompt } from 'enquirer';
-import { shellCommander } from '../shell-commander'
-import { logger } from '../logger'
+import { gitPublisher } from '../git-publisher'
 
 (async () => {
   program
     .description('Publish updates to Gordion GIT repository')
-    .action(publishToGIT)
+    .action((name, options, command) => {
+      gitPublisher.publishToGIT('gordion')
+    })
     await program.parseAsync()
   })()
-  
-  async function gitStatus() {
-    const status = await shellCommander.exec('cd node_modules/gordion && git status')
-    return status
-  }
-  
-  async function selectFilesToPublish() {
-    const result = await shellCommander.exec('cd node_modules/gordion && git diff --name-only', {}, true)
-    let files = result.stdout.trim().split('\n')
-    if(files.length == 0 || (files.length == 1 && files[0] == '')) {
-      return false
-    } else {
-      files.unshift('ALL')
-    }
-    try {
-      const input: Record<string,any> = await prompt({
-        type: 'multiselect',
-        name: 'selectedFiles',
-        //initial: true,
-        message: 'Select files to be published (ESC to abort)',
-        choices: files
-      })
-      if(input.selectedFiles.indexOf('ALL') !== -1) {
-        input.selectedFiles = result.stdout.split(/\r?\n/)
-      }
-      return input.selectedFiles
-    } catch(e) {
-      return false
-    }
-  }
-
-  async function askGitCommitMessage() {
-    const input: Record<string,any> = await prompt({
-      type: 'input',
-      name: 'message',
-      initial: 'Gordion commit',
-      message: 'GIT commit message'
-    })
-    return input.message
-  }
-
-  async function gitPublish(filesToPublish: [string],message: string) {
-    const commit = await shellCommander.exec('cd node_modules/gordion && git add ' + filesToPublish.join(' ') + ' && git commit -m "' + message + '" && git push')
-    return commit
-  }
-  
-  async function publishToGIT() {
-    await gitStatus()
-    const filesToPublish = await selectFilesToPublish()
-    if(filesToPublish !== false) {
-      const message = await askGitCommitMessage()
-      const commit = await gitPublish(filesToPublish, message)
-    }
-  }
