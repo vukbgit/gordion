02b7448d21afc99fa32b679509f991de60d17bde
vukbgit <info@vuk.bg.it>
2021-09-24 15:38:59 +0200

    init-gordion-repository binary

---
+++ dist/bin/init-gordion-repository.js
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+"use strict";
+
+var _commander = require("commander");
+
+var _publisher = require("../publisher");
+
+const program = new _commander.Command();
+
+(async () => {
+  program.description('Init Gordion GIT repository').action((name, options, command) => {
+    _publisher.publisher.initGITRepository('gordion');
+  });
+  await program.parseAsync();
+})();
---
+++ src/bin/init-gordion-repository.ts
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+import { Command } from 'commander';
+const program = new Command();
+import { publisher } from '../publisher'
+
+(async () => {
+  program
+    .description('Init Gordion GIT repository')
+    .action((name, options, command) => {
+      publisher.initGITRepository('gordion')
+    })
+    await program.parseAsync()
+  })()
